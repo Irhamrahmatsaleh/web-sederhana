7f125ab06ebc9983561e3d6f4d3f73262af8f6b6
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-06-25 11:58:51 +0700

    chore: bersihkan template, siap penembangan login/register

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Website Sederhana - Proyek Baru</h1>
+      <p>Siap untuk fitur login & register</p>
+    </div>
+  )
+}
+
+export default App
